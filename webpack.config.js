f8fdda0092d4330476eaf8e02dbb37934b62fa85
Dmitry <dmitrijharitonov408@gmail.com>
2019-09-01 19:28:09 +0500

    setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = (env) => {
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test'})
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development'})
+}
+
+module.exports = (env) => {   
+    const CSSExtract = new ExtractTextPlugin('styles.css'); 
     const isProduction = env === 'production';
 
     return {
             entry: './src/app.js',
             output: {
-                path: path.join(__dirname, 'public', 'dist'),
+                path: path.join(__dirname, 'public', 'dist'),    
                 filename: 'bundle.js'
             },
             module: {
@@ -37,8 +46,17 @@ module.exports = (env) => {
                     })
                 }]
             },
-            plugins: [
-                CSSExtract
+            plugins: [ 
+                CSSExtract,
+                new webpack.DefinePlugin({
+                    'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                    'process.env.FIREBASE_AUTH_DOMAINprocess.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                    'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                    'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                    'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                    'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                    'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
+                })
             ],
             devtool: isProduction ? 'source-map' : 'inline-source-map',
             devServer: {
